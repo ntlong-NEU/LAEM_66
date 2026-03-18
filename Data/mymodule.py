8f95217071109547ee1e1cb6d1145a41501983e3
Nguyen Tuan Long <ntlong@neu.edu.vn>
2026-03-18 11:34:45 +0700

    1803

---
+++ Data/mymodule.py
@@ -0,0 +1,14 @@
+def count_fre_word(text:str)->dict:
+    '''docstring'''
+    words = text.split()
+    fre_words = {}
+    for w in words:
+        if w in fre_words:
+            fre_words[w] += 1
+        else:
+            fre_words[w] = 1
+    return fre_words
+
+DATA = ['Hello LAEM', {"score":[6,5,4,5,4,9]}]
+
+print('My module is imported')
